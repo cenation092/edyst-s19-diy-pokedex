321dda278c013b95d7c97ab2bb4bcca53be836e0
cenation092 <cenation092@gmail.com>
2019-04-17 18:17:39 +0530

    update

--- backend/README.md
+++ backend/README.md
@@ -1,4 +1,16 @@
-This API return the details about the pokémons with ID.
+This repo contains API that return the details about the pokémons of the required ID using flask framework.
+
+## Steps to run this API
+
+	1. Clone this repo to your local machine using [this linl](https://github.com/cenation092/edyst-s19-diy-pokedex.git).
+
+	2. Then make virtual environment in the cloned repo on you local machine [read this to make virtual environment](https://blog.ruanbekker.com/blog/2018/12/09/python-flask-tutorial-series-setup-a-python-virtual-environment-p2/).
+
+	3. Then install all reuired packages which are given in requirment.txt file.
+
+	4. Then run the `app.py` file. 
+
+	5. 
 
 **Route** = `/api/pokemon/:id` where `:id` is an integer and can vary from **1** to **807**
 
--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,43 @@
 import json
-from flask import Flask
-from flask import jsonify
 import requests
+from flask import Response
+from flask import Flask, render_template
 
+# Instance of the Flask class for the API
 app = Flask(__name__)
 
+# Error handeler for invalid pokemon id
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+# Error handeler for invalid route
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+# Routing for valid pokemon id , Example: http://127.0.0.1:8006/api/pokemon/1
 @app.route('/api/pokemon/<int:num>')
 def index(num):
-	data = {}
-	if num > 807:
-		data['pokemon'] = "Missing"
-		json_data = json.dumps(data)
-		return json_data
+	data = {} # dictonary for holding required pokemon data
+	URL = "https://pokeapi.co/api/v2/pokemon/" # external API to get data of required pokemon
+	URL = URL + str(num) # Add req Id of pokemon to the external API
+	response = requests.get(url = URL) # request for getting data of requred pokemon
+	responseData = response.json() # convert the data into JSON
 
-	URL = "https://pokeapi.co/api/v2/pokemon/"
-	URL = URL + str(num)
-	response = requests.get(url = URL)
-	responseData = response.json()
-	data['id'] = responseData['id']
+	# Extract useful information from data which is provide by the external API
+	data['id'] = responseData['id'] 
 	data['name'] = responseData['name']
 	data['sprite'] = responseData['sprites']['back_default']
+
+	# dictonary to hold whole pokemon
 	pokemon = {}
 	pokemon['pokemon'] = data
+
+	#convert whole data into JSON
 	json_data = json.dumps(pokemon)
 
+	#return required data 
 	return json_data
 
 
